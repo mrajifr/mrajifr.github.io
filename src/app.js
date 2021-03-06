5d822f9445111c54f6494c6ed7ae2cb598b59041
mrajifr <mrajifr@gmail.com>
2021-03-06 14:48:39 +0700

    first commit

---
+++ src/app.js
@@ -0,0 +1,37 @@
+const express = require('express'),
+      path = require('path'),
+      morgan = require('morgan'),
+      mysql = require('mysql'),
+      myConnection = require('express-myconnection');
+
+const app = express();
+
+// importing routes
+const customerRoutes = require('./routes/customer');
+
+// settings
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// middlewares
+app.use(morgan('dev'));
+app.use(myConnection(mysql, {
+  connectionLimit: 1000,
+  host: 'db4free.net',
+  user: 'ajiuser', // use your mysql username.
+  password: '123456qwerty', // user your mysql password.
+  database: 'ajidbtest',
+}, 'single'));
+app.use(express.urlencoded({extended: false}));
+
+// routes
+app.use('/', customerRoutes);
+
+// static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// starting the server
+app.listen(app.get('port'), () => {
+  console.log(`server on port ${app.get('port')}`);
+});
